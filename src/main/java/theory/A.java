84ccc83174d898ecaa0c2fbfef36a1a99a3c71cf
ravivarmachaluvadi <ravivarmachaluvadi@gmail.com>
2021-05-21 17:04:24 +0530

    May 2021 21

---
+++ src/main/java/theory/A.java
@@ -0,0 +1,37 @@
+package theory;
+
+class A
+{
+    void m2()
+    {
+        System.out.println("Class A method m2");
+        m1();
+    }
+
+    void m1()
+    {
+        System.out.println("Class A method m1");
+    }
+}
+ 
+class B extends A
+{
+    void m1()
+    {
+        System.out.println("Class B method m1");
+    }
+}
+ 
+class MainClass
+{
+    public static void main(String[] args)
+    {
+        A a = new B();
+
+        a.m2();
+
+    }
+}
+
+    //Class A method m2
+    //Class B method m1
